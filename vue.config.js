87c7f1c15756d67d4f650f54059a42b142266212
A-little-squirrel <48399477+A-little-squirrel@users.noreply.github.com>
2020-03-16 20:45:59 +0800

    构建项目、配置全局样式、路由、elementui

---
+++ vue.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+module.exports = {
+  // 部署应用包时的基本 URL,用法和 webpack 本身的 output.publicPath 一致
+  publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
+  // 输出文件目录
+  outputDir: process.env.NODE_ENV === 'production' ? 'dist' : 'devdist',
+  // eslint-loader 是否在保存的时候检查
+  lintOnSave: false,
+  // 是否使用包含运行时编译器的 Vue 构建版本
+//   runtimeCompiler: false,
+  // 生产环境是否生成 sourceMap 文件
+  // 生成的 HTML 中的 <link rel="stylesheet"> 和 <script> 标签上启用 Subresource Integrity (SRI)
+//   integrity: false,
+  // webpack相关配置
+  chainWebpack: (config) => {
+  },
+  configureWebpack: (config) => {
+    // config.resolve = {//配置解析别名
+    //     extensions: ['.js', '.json', '.vue'],
+    //     alias: {
+    //         '@': path.resolve(__dirname, './src'),
+    //         'public': path.resolve(__dirname, './public'),
+    //         'components': path.resolve(__dirname, './src/components'),
+    //         'common': path.resolve(__dirname, './src/common'),
+    //         'api': path.resolve(__dirname, './src/api'),
+    //         'views': path.resolve(__dirname, './src/views'),
+    //         'data': path.resolve(__dirname, './src/data')
+    //     }
+    // }
+  },
+  productionSourceMap: false,
+  // css相关配置
+  css: {
+    // 是否分离css（插件ExtractTextPlugin）
+    extract: false,
+    // 是否开启 CSS source maps
+    sourceMap: false,
+    // css预设器配置项
+    loaderOptions: {
+      sass: {
+        prependData: `@import "./src/styles/main.scss";`
+      }
+    },
+    // 是否启用 CSS modules for all css / pre-processor files.
+    requireModuleExtension: true,
+  },
+  // 是否使用 thread-loader
+  parallel: require('os').cpus().length > 1,
+  // PWA 插件相关配置,see https://github.com/vue.js/vue-cli/tree/dev/packages/%40vue/cli-pugin-pwa
+  pwa: {},
+  // webpack-dev-server 相关配置
+  devServer: {
+    open: false,// 编译完成是否打开网页
+    host: '0.0.0.0',// 指定使用地址，默认localhost，0.0.0.0代表可以被外界访问
+    port: 8080,// 访问端口
+    https: false,// 编译失败时刷新页面
+    hot: true,// 开启热加载
+    hotOnly: false,
+    // http 代理配置
+    proxy: null,
+    overlay:{
+        warnings: true,
+        errors: true
+    },
+    before: app => {}
+  },
+  // 第三方插件配置
+  pluginOptions: {}
+}
